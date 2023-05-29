5651b9b63dd57471a9c99d61b30c974b6259a782
Ekejiuba Prince <dennisekoxy@gmail.com>
2023-05-29 05:59:12 +0100

    Added additional styling and styled components using the material UI library

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React,{useState} from "react";
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 function CreateArea(props) {
     const [input, setInput] = useState({
         title:'',
         content: ''
     })
+    const [click,setClick] = useState(false)
     function inputHandler(e){
     const {value,name} = e.target;
     setInput((prevValue)=>{
@@ -17,17 +21,22 @@ function CreateArea(props) {
     }
   return (
     <div>
-      <form>
-        <input onChange={inputHandler} name="title" placeholder="Title" value={input.title}/>
-        <textarea onChange={inputHandler} name="content" placeholder="Take a note..." rows="3" value={input.content} />
-        <button onClick={(e)=>{
+      <form className="create-note">
+
+        <input onChange={inputHandler} hidden={click?false:true} name="title" placeholder="Title" value={input.title}/>
+        <textarea onClick={()=>{
+            setClick(true)
+        }} onChange={inputHandler} name="content" placeholder="Take a note..." rows={click?3:1} value={input.content} />
+        <Zoom in={click?true:false}>
+        <Fab onClick={(e)=>{
             e.preventDefault()
             props.createNote(input)
             setInput({
                 title:'',
                 content: ''
             })
-        }}>Add</button>
+        }}><AddCircleIcon/></Fab>
+        </Zoom>
       </form>
     </div>
   );
